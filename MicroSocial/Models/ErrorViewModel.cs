179e99df7ac68bee9591630489c59af5696ab46d
Daniel Mitrache <mitracheadaniel@gmail.com>
2025-12-15 23:22:43 +0200

    initial commit

---
+++ MicroSocial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MicroSocial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
